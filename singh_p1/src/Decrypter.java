02de2295e8fb81320d45c0c859568a98d32f3719
Matthew Singh <mattsingh@hotmail.com>
2020-09-10 00:54:19 -0400

    added methods to class

--- singh_p1/src/Decrypter.java
+++ singh_p1/src/Decrypter.java
@@ -1,3 +1,37 @@
 public class Decrypter {
+  public String decrypt(String ciphertext) {
+    int[] arr = stringToIntArray(ciphertext);
 
+    for(int i = 0; i < arr.length; i++) {
+      arr[i] = (arr[i] - 7 + 10) % 10;
+    }
+
+    arr = swap(arr);
+
+    return toString(arr);
+  }
+
+  private int[] swap(int[] arr) {
+    int temp;
+    for(int i = 0; i + 2 < arr.length; i++) {
+      temp = arr[i];
+      arr[i] = arr[i + 2];
+      arr[i + 2] = temp;
+    }
+    return arr;
+  }
+
+  private int[] stringToIntArray(String str) {
+    int[] arr = new int[str.length()];
+    for(int i = 0; i < str.length(); i++)
+      arr[i] = Integer.parseInt(str.substring(i, i + 1));
+    return arr;
+  }
+
+  private String toString(int[] arr) {
+    String str = "";
+    for(int i : arr)
+      str = str + i;
+    return str;
+  }
 }
