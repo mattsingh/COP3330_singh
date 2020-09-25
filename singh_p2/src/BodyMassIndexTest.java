4cffc8bb97bc008ef8f55dda744248a9d60b077d
Matthew Singh <mattsingh@hotmail.com>
2020-09-24 23:28:05 -0400

    Added JUnit Tests for bmi category and bmi calculation

--- singh_p2/src/BodyMassIndexTest.java
+++ singh_p2/src/BodyMassIndexTest.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 public class BodyMassIndexTest {
   @Test
   public void testEachBMICategory() {
@@ -14,5 +13,14 @@ public class BodyMassIndexTest {
     assertEquals(test2.category, "Normal");
     assertEquals(test3.category, "Overweight");
     assertEquals(test4.category, "Obesity");
+  }
+
+  @Test
+  public void testCalculateBMI() {
+    BodyMassIndex test1 = new BodyMassIndex(70, 150);
+    BodyMassIndex test2 = new BodyMassIndex(70, 200);
+
+    assertEquals(21.52, test1.bmi, .01);
+    assertEquals(28.69, test2.bmi, .01);
   }
 }
