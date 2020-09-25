5368b3996f689ff938f0ce76f04367120a25b84e
Matthew Singh <mattsingh@hotmail.com>
2020-09-24 22:46:45 -0400

    BMI Category JUnit Test

--- singh_p2/src/BodyMassIndexTest.java
+++ singh_p2/src/BodyMassIndexTest.java
@@ -1,2 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class BodyMassIndexTest {
+  @Test
+  public void testEachBMICategory() {
+    BodyMassIndex test1 = new BodyMassIndex(70, 100);
+    BodyMassIndex test2 = new BodyMassIndex(70, 150);
+    BodyMassIndex test3 = new BodyMassIndex(70, 200);
+    BodyMassIndex test4 = new BodyMassIndex(70, 250);
+
+    assertEquals(test1.category, "Underweight");
+    assertEquals(test2.category, "Normal");
+    assertEquals(test3.category, "Overweight");
+    assertEquals(test4.category, "Obesity");
+  }
 }
