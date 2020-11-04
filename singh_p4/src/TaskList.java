e447141a11f713acca0d64b7da5ff8a649bceb17
Matthew Singh <mattsingh@hotmail.com>
2020-11-03 21:07:46 -0500

    prototyped TaskList class

--- singh_p4/src/TaskList.java
+++ singh_p4/src/TaskList.java
@@ -1,2 +1,36 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TaskList {
+  private static Scanner in = new Scanner(System.in);
+  ArrayList<TaskItem> tasks = new ArrayList<>();
+
+  public TaskList() {
+    System.out.println("new task list has been created");
+  }
+
+  public void add() {
+    tasks.add(new TaskItem());
+  }
+
+  public TaskItem edit(int index) {
+    System.out.println("Enter a new title for task " + index + ": ");
+    tasks.get(index).setTitle(in.nextLine());
+    System.out.println("Enter a new description for task " + index + ": ");
+    tasks.get(index).setDescription(in.nextLine());
+    System.out.println("Enter a new task due date (YYYY-MM-DD) for task " + index + ": ");
+    tasks.get(index).setDueDate(LocalDate.parse(in.nextLine()));
+    return tasks.get(index);
+  }
+
+  public void print() {
+    System.out.println(
+        "Current Tasks\n" +
+        "-------------\n");
+
+    for(int i = 0; i < tasks.size(); i++)
+      System.out.println(i + ") " + tasks.get(i));
+    in.nextLine();
+  }
 }
