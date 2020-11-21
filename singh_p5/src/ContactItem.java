2670faa6bbb65eed14a80ffb41c1adb4869b796d
Matthew Singh <mattsingh@hotmail.com>
2020-11-21 18:49:25 -0500

    created necessary variables for ContactItem and contructor

--- singh_p5/src/ContactItem.java
+++ singh_p5/src/ContactItem.java
@@ -1,2 +1,14 @@
 public class ContactItem {
+
+  private String firstName;
+  private String lastName;
+  private String phoneNumber;
+  private String emailAddress;
+
+  public ContactItem(String firstName, String lastName, String phoneNumber, String emailAddress) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.emailAddress = emailAddress;
+  }
 }
