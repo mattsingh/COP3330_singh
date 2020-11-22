ccadcc2296f50b6b11ab18af056bf731a4bb4959
Matthew Singh <mattsingh@hotmail.com>
2020-11-21 19:35:54 -0500

    created add, edit, remove, get, and getSize methods

--- singh_p5/src/ContactList.java
+++ singh_p5/src/ContactList.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class ContactList {
+
+  ArrayList<ContactItem> contacts = new ArrayList<>();
+
+  public void add(ContactItem item) {
+    contacts.add(item);
+  }
+
+  public ContactItem edit(int index, String firstName, String lastName, String phoneNumber, String emailAddress) {
+    contacts.set(index, new ContactItem(firstName, lastName, phoneNumber, emailAddress));
+  }
+
+  public ContactItem remove(int index) {
+    contacts.remove(index);
+  }
+
+  public ContactItem get(int index) {
+    return contacts.get(index);
+  }
+
+  public int getSize() {
+    return contacts.size();
+  }
 }
