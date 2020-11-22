381c97a0805cb9a2a97475976a81a0bc503ace8f
Matthew Singh <mattsingh@hotmail.com>
2020-11-21 20:20:47 -0500

    created appMenu method

--- singh_p5/src/AppSelector.java
+++ singh_p5/src/AppSelector.java
@@ -1,2 +1,49 @@
+import java.util.Scanner;
+
 public class AppSelector {
+  private final Scanner in = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    AppSelector as = new AppSelector();
+    as.appMenu();
+    TaskApp taskApp = new TaskApp();
+    taskApp.mainMenu();
+  }
+
+  private void appMenu() {
+    while(true) {
+      int input = -1;
+
+      System.out.print(
+          "Select Your Application\n" +
+          "-----------------------\n" +
+          "\n" +
+          "1) task list\n" +
+          "2) contact list\n" +
+          "3) quit\n" +
+          "\n");
+
+      do {
+        System.out.print("> ");
+        try {
+          input = Integer.parseInt(in.nextLine());
+        } catch (NumberFormatException e) {
+          System.out.println("WARNING: input must be an integer");
+        }
+      } while(input < 1 || input > 3);
+
+      switch(input) {
+        case 1:
+          TaskApp tApp = new TaskApp();
+          tApp.mainMenu();
+          break;
+        case 2:
+          ContactApp cApp = new ContactApp();
+          cApp.mainMenu();
+          break;
+        case 3:
+          return;
+      }
+    }
+  }
 }
