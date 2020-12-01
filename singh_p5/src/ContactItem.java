45bbd6b3365442e377db1407acb9e9e7a6e645e7
Matthew Singh <mattsingh@hotmail.com>
2020-11-30 22:33:33 -0500

    deletes ContactItem setters as they were unused

--- singh_p5/src/ContactItem.java
+++ singh_p5/src/ContactItem.java
@@ -28,31 +28,16 @@ public class ContactItem implements Serializable {
     return firstName;
   }
 
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
   public String getLastName() {
     return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
   }
 
   public String getPhoneNumber() {
     return phoneNumber;
   }
 
-  public void setPhoneNumber(String phoneNumber) {
-    this.phoneNumber = phoneNumber;
-  }
-
   public String getEmailAddress() {
     return emailAddress;
   }
 
-  public void setEmailAddress(String emailAddress) {
-    this.emailAddress = emailAddress;
-  }
 }
