96ac0cc9b4befb1dcdcc08c5be0774d48ff0dfa6
Matthew Singh <mattsingh@hotmail.com>
2020-11-30 20:43:40 -0500

    added getter and setter methods

--- singh_p5/src/ContactItem.java
+++ singh_p5/src/ContactItem.java
@@ -11,4 +11,36 @@ public class ContactItem {
     this.phoneNumber = phoneNumber;
     this.emailAddress = emailAddress;
   }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public void setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
 }
